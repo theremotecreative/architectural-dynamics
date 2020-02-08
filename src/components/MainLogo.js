851c6ed5c5f7d694f85b438d1e2870f1800441cd
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2020-02-08 14:06:31 -0600

    need logo

--- src/components/MainLogo.js
+++ src/components/MainLogo.js
@@ -1,27 +1,15 @@
-import { StaticQuery, graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import React from "react"
 import styled from 'styled-components'
 
 const MainLogo = () => (
-    <StaticQuery
-      query={graphql`
-        query {
-          wordpressSiteMetadata {
-            name
-          }
-        }
-      `}
-  
-      render={data => (
         <h1 className="mainLogo">
             <MainLogoLink 
             to="/"
             >
-                {data.wordpressSiteMetadata.name}
+                <NeedLogo>[Need Logo Here]</NeedLogo>
             </MainLogoLink>
         </h1>
-      )}
-    />
   )
 
 const MainLogoLink = styled(Link)`
@@ -30,6 +18,17 @@ const MainLogoLink = styled(Link)`
   @media(max-width:800px) {
     color: #fff;
   }
+`
+
+const NeedLogo = styled.p`
+  font-size: 2.25rem;
+  padding: 0;
+  margin: 0;
+  color: inherit;
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  font-weight: 700;
+  text-rendering: optimizeLegibility;
+  line-height: 1.1;
 `
 
   export default MainLogo
